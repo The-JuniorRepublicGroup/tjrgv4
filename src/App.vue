9ade8b61b98a3d6de865ff501e2ce7d5239c4119
freeCCaccount <rokafor89@gmail.com>
2017-06-08 17:43:53 -0400

    scaffold vue with nav

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+
+export default {
+  components: {
+    'navbar':Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
